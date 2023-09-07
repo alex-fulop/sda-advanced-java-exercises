cc9f2f2f3f4d2e98e5315288a0d2e3e078b8f608
Fulop Alex-Dominic <Alex.Fulop@swisscom.com>
2023-09-07 18:00:57 +0300

    Lesson 7: Collections

--- src/main/java/collections/Main.java
+++ src/main/java/collections/Main.java
@@ -1,19 +1,101 @@
 package collections;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
 
 public class Main {
 	public static void main(String[] args) {
-		List<String> fruits = new ArrayList<>();
-		fruits.add("Apple");
-		fruits.add("Banana");
-		fruits.add("Orange");
 
-		Iterator<String> iterator = fruits.iterator();
-		int index = 0;
+		Map<String, String> countries = new TreeMap<>();
+		Map<String, String> countries2 = new TreeMap<>();
 
+		countries2.put("RO", "Romania");
+		countries.put("US", "United States");
+		countries.put("BR", "Brazil");
+		countries.put("ES", "Spain");
+
+		countries.putAll(countries2);
+
+		System.out.println(countries.get("RO"));
+		System.out.println(countries);
+
+		System.out.println("Keys: " + countries.keySet());
+		System.out.println("Values: " + countries.values());
+
+//		Set<String> animal = new TreeSet<>();
+//		animal.add("Dog");
+//		animal.add("Tiger");
+//		animal.add("Cat");
+//		animal.add("Lion");
+//
+//		animal.remove("Lion");
+//		animal.add("Lion");
+//		animal.add("Lion");
+//
+//		System.out.println(animal);
+
+//		String[] newAnimals = {"Dolphin", "Giraffe"};
+//		animal.addAll(Arrays.asList(newAnimals));
+//
+//		String[] array = animal.toArray(new String[0]);
+//
+//		System.out.println(Arrays.toString(array));
+//
+//		for(int i = 0; i< array.length; i++) {
+//			System.out.println(array[i]);
+//		}
+
+//		System.out.println(animal);
+//		System.out.println(animal.size());
+//		while(iterator.hasNext()) {
+//			System.out.print(iterator.next() + ", ");
+//		}
+
+
+//		Queue<String> names = new LinkedList<>();
+
+//		names.offer("Ana");
+//		names.offer("Dan");
+//		names.offer("Andrei");
+//		names.add("Claud");
+//		names.add("David");
+
+//		names.poll();
+//		names.remove();
+
+//		System.out.println(names);
+//
+//		names.clear();
+
+//		System.out.println(names.poll());
+//		names.poll()
+//		while (!names.isEmpty()) {
+//			System.out.print(names.remove() + " ");
+//		}
+
+
+//		List<String> fruits = new ArrayList<>();
+//		List<String> fruits2 = new ArrayList<>();
+//
+//		fruits2.add("pineapple");
+//		fruits2.add("kiwi");
+//		fruits2.add("peach");
+//
+//		fruits.add("Apple");
+//		fruits.add("Banana");
+//		fruits.add("Orange");
+//
+//		fruits2.addAll(fruits);
+
+//		fruits2.clear();
+
+//		System.out.println("The size of the fruits list is: " + fruits2.size());
+
+//		fruits.remove("Banana");
+
+//		Iterator<String> iterator = fruits.iterator();
+//		int index = 0;
 //		while(iterator.hasNext()) {
 //			System.out.println(fruits.get(index));
 //			iterator.next();
@@ -21,11 +103,10 @@ public class Main {
 //				index++;
 //			}
 //		}
+//		System.out.println(fruits2);
 
-		System.out.println(fruits);
-
-		for (String fruit : fruits) {
-			System.out.println(fruit.toUpperCase());
-		}
+//		for (String fruit : fruits) {
+//			System.out.println(fruit.toUpperCase());
+//		}
 	}
 }
